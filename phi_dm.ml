2f4004a85d422c3cf1d279585dd7aee59d580dfc
Naoki Yoshinaga <thats_525@mail.goo.ne.jp>
2012-06-06 23:08:14 +0900

    change event execute order

--- phi_dm.ml
+++ phi_dm.ml
@@ -4,7 +4,7 @@ let rec main_loop event_list =
   let client_message_event_list = Client_manager.fetch_messages () in
   let event_list =
     Chara_manager.event_dispatch
-      ~event_list:(List.concat [clock_event_list; event_list; client_message_event_list])
+      ~event_list:(List.concat [event_list; client_message_event_list; clock_event_list])
   in
   main_loop event_list
 ;;
