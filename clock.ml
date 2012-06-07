4e6e0050387214b7b6b090213a66eec201804c66
Naoki Yoshinaga <thats_525@mail.goo.ne.jp>
2012-06-07 20:25:56 +0900

    set frame-rate to 0.1 sec

--- clock.ml
+++ clock.ml
@@ -3,7 +3,7 @@ let current_tick = ref 0;;
 let tick () =
   current_tick := !current_tick + 1;
   let event_list =
-    if !current_tick mod 10 = 0
+    if !current_tick mod 200 = 1
     then [Event.Npc_appear]
     else []
   in
--- phi_dm.ml
+++ phi_dm.ml
@@ -1,15 +1,18 @@
-let rec main_loop event_list =
-  Thread.delay 1.0;
+let rec main_loop event_list prev_time =
   let clock_event_list = Clock.tick () in
   let client_message_event_list = Client_manager.fetch_messages () in
   let event_list =
     Chara_manager.event_dispatch
       ~event_list:(List.concat [event_list; client_message_event_list; clock_event_list])
   in
-  main_loop event_list
+  Thread.delay (max 0.0 (0.1 -. ((Sys.time ()) -. prev_time)));
+  print_endline "-----------------";
+  print_endline (string_of_float (max 0.0 (0.1 -. ((Sys.time ()) -. prev_time))));
+  print_endline (string_of_float (Sys.time ()));
+  main_loop event_list (Sys.time ())
 ;;
 
 let _ =
   Random.self_init ();
-  main_loop []
+  main_loop [] (Sys.time ())
 ;;
